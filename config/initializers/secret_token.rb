4d9940d89590749b534194ca0f4810cd949ac47b
Alexandre Girard <alx.girard@gmail.com>
2011-01-15 13:05:13 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jadmin::Application.config.secret_token = 'eea2aeebca924c35227b2c496b8bcb539964b551e25a60ab8abd34ddc01ff51d71db7f8b4df4fd1cdd8afa19e177deaaf22abecf2655243f0024524ed777e6ad'
