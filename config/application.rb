8661c964414c44fe884dfa6dacb94290990fdd6c
Alexandre Girard <alx.girard@gmail.com>
2011-01-16 15:48:51 +0100

    remove comma

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module Jadmin
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w(wmd, showdown)
+    config.action_view.javascript_expansions[:defaults] = %w(wmd showdown)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
